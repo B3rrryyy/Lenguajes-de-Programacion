5185995b22f4cebafb4705bff240ea5f5585050d
b3rrryyy <ga459354@uaeh.edu.mx>
2025-09-06 19:44:47 -0600

    Update and rename carpeta1/carpeta2/reloj.cpp to reloj.cpp

--- reloj.cpp
+++ reloj.cpp
@@ -1,6 +1,3 @@
-// Este codigo ha sido generado por el modulo psexport 20230904-w32 de PSeInt.
-// Es posible que el codigo generado no sea completamente correcto. Si encuentra
-// errores por favor reportelos en el foro (http://pseint.sourceforge.net).
 
 #include<iostream>
 #include<ctime>
@@ -31,4 +28,5 @@ void esperar(double t) {
 		e = 1000*double(clock()-t0)/CLOCKS_PER_SEC;
 	} while (e<t);
 }
+
 
